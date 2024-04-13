7d9ce954b9fbfb88ea518a2160fcc9b7ab6e0c57
shanchuan <1537521192@qq.com>
2024-04-14 00:14:18 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(quantity:i32)->i32 {
+    let price = if quantity >40{
+        1
+    }else{
+        2
+    };
+    price *quantity
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
